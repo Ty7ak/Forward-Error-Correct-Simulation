092b3eecc2f48eb19b03671cfdc35727ccee05a8
Ty7ak <44756071+Ty7ak@users.noreply.github.com>
2020-04-21 16:58:58 +0200

    Update NiDSC.m

--- NiDSC.m
+++ NiDSC.m
@@ -1,21 +1,16 @@
-n_bits = 20;
-rows = 1;
+%%%GENERATOR%%%
 
-%message we want to encode
+n_bits = 10000;
+rows = 1;
 init_bits = randi([0 1], rows, n_bits);
 
+%%%ENCODING%%%%
+
 %tripple bits encoding
-triple_bits = repelem(init_bits, 3);
+triple_bits_encoded = repelem(init_bits, 3);
 
 %hamming encoding
-n = 7;
-k = 4;
-A = [ 1 1 1; 1 1 0; 1 0 1; 0 1 1 ];        
-G = [ eye(k) A ];
-H = [ A' eye(n-k) ];
-msg_mtrx = transpose(reshape(init_bits, k, n_bits/k));
-hamm_bits_mtrx = mod(msg_mtrx*G, 2);
-hamm_bits_vec = hamm_bits_mtrx(:);
+hamming_encoded = encode(init_bits, 7, 4, 'hamming/binary');
 
 %bch encoding
 m = 4;
@@ -25,21 +20,25 @@ bits_k_mtrx = reshape(init_bits, n_bits/k, k);
 msgTx = gf(bits_k_mtrx);
 bch_encoded = bchenc(msgTx, n, k);
 
-%getting bch bits for array representation
-m = 1;
-prim_poly = 3;
-bch_bits = gf2dec(bch_encoded, m, prim_poly);
+%%%CHANNELS%%%
 
 %choosing what coding we want to use to transport bits
-%data = bch_bits;
-%data = hamm_bits_vec;
-data = triple_bits;
+data = bch_encoded;
+%data = hamming_encoded;
+%data = triple_bits_encoded;
 
 %bsc channel
-probability = 0.3;
+probability = 0.1;
 ndata = bsc(data, probability);
 
-%decoding triple bits
+%Gilbert model%
+
+%%%DECODING%%%%
+
+%%%triple bits decoding%%%
+
+%{
+
 zeros = 0;
 ones = 0;
 decoded_data = [];
@@ -51,7 +50,7 @@ for bit = ndata
     end
     if zeros + ones == 3
         if zeros > ones
-            decoded_data = [decoded_data, 0]; %#ok<*AGROW>
+            decoded_data = [decoded_data, 0]; 
         else
             decoded_data = [decoded_data, 1];
         end
@@ -59,23 +58,28 @@ for bit = ndata
         ones = 0;
     end
 end
-[number, ratio] = biterr(init_bits, decoded_data);
+triple_bits_BER = biterr(init_bits, decoded_data)/n_bits
 
-subplot(2, 2, 1);
-plot(init_bits);
-title("Init bits");
+%}
 
-subplot(2, 2, 2);
-plot(triple_bits);
-title("Triple bits");
+%%%hamming decoding%%%
 
-subplot(2, 2, 3);
-plot(hamm_bits_vec);
-title("Hamming bits");
+%{
+hamming_decoded = decode(ndata, 7, 4, 'hamming/binary');
+hamming_BER = biterr(init_bits, hamming_decoded)/n_bits
+%}  
 
-subplot(2, 2, 4);
-plot(bch_bits);
-title("BCH bits");
+%%%bch decoding%%%
+
+%
+bch_decoded = bchdec(ndata, n, k);
+m = 1;
+prim_poly = 3;
+bch_bits = gf2dec(bch_decoded, m, prim_poly);
+bch_BER = biterr(bch_bits, init_bits)/n_bits
+%}
+
+%%%%%%%%%%%%%%%%%%%%%%%%
 
 %turning gf array to decimal array
 function [DecOutput] = gf2dec(GFInput,m,prim_poly)
@@ -90,3 +94,4 @@ for i=1:length(GFInput)
     end
 end
 end
+
